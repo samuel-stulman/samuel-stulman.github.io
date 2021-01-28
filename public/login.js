593a71dcdb587f30ad5a370430d9e5373cdcafcf
Samuel Stulman <samuel.stulman@berkeley.edu>
2021-01-28 01:15:38 -0800

    add a alert test

--- public/login.js
+++ public/login.js
@@ -1,6 +1,8 @@
 function validate(){
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    alert(username);
+    var password = document.getElementById("password").value;
+    alert(password);
 	if(username == "sam" && password == "sam"){
 		window.location.href = "/static/main.html";
 		return false;
