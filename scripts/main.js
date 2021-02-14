955e1bb7d836640c13cd9ffa4c361bfeae06d40d
Samuel Stulman <samuel.stulman@berkeley.edu>
2021-02-13 21:40:17 -0800

    test with alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function change() {
     var myImage = document.getElementById('profile');
-
+    Window.alert("This is a test");
     if (myImage.src === "images/IMG-2716.JPG") {
         myImage.src = "images/IMG-3665.jpg";
     } else {
