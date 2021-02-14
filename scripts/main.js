afa02ece1bac40e121ae701171c2b8a16e3588be
Samuel Stulman <samuel.stulman@berkeley.edu>
2021-02-13 21:35:13 -0800

    maybe this will work

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,9 @@
-
-
 function change() {
     var myImage = document.getElementById('profile');
 
-    var mrSrc = myImage.getAttribute('src');
-    if (mySrc === "images/IMG-2716.JPG") {
+    if (myImage.src === "images/IMG-2716.JPG") {
         myImage.src = "images/IMG-3665.jpg";
     } else {
         myImage.src = "images/IMG-2716.JPG";
     }
-    
 }
